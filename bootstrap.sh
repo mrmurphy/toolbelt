3c08d3da48b836667851a658b84fbf88c81030a5
Murphy Randle <murphyspublic@gmail.com>
2013-10-02 12:35:46 -0600

    Add deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+# Make sure we have the necessary deps:
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   sudo apt-get install -y git vim
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   brew install git vim
+fi
 
 # Install Prezto
 git clone --recursive https://github.com/murphyrandle/prezto.git "$HOME/.zprezto"
-zsh _zsh_bootstrap.sh
+zsh ./_zsh_bootstrap.sh
 chsh -s /bin/zsh
 
 # Install iVim
